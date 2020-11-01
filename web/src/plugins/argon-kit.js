3a726e1a2553cd294fc2f7a3ee18fd8e53187215
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-11-01 10:25:24 -0500

    webapp cleanup

--- web/src/plugins/argon-kit.js
+++ web/src/plugins/argon-kit.js
@@ -1,10 +1,10 @@
-import '@/assets/scss/argon-design-system.scss';
-import '@/assets/vendor/nucleo/css/nucleo-icons.css';
-import '@/assets/vendor/font-awesome/css/font-awesome.css';
+import "@/assets/scss/argon-design-system.scss";
+import "@/assets/vendor/nucleo/css/nucleo-icons.css";
+import "@/assets/vendor/font-awesome/css/font-awesome.css";
 
-import globalComponents from './globalComponents';
-import globalDirectives from './globalDirectives';
-import globalMixins from './globalMixins';
+import globalComponents from "./globalComponents";
+import globalDirectives from "./globalDirectives";
+import globalMixins from "./globalMixins";
 
 export default {
   install(Vue) {
