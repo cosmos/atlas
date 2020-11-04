3aeb5be04ec4ae9bd012c096a7784e91cee9435a
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-11-04 18:30:02 -0500

    schema+

--- db/migrations/000005_add_email_confirmation.up.sql
+++ db/migrations/000005_add_email_confirmation.up.sql
@@ -4,8 +4,8 @@ ADD COLUMN IF NOT EXISTS email_confirmed BOOLEAN DEFAULT FALSE;
 CREATE TABLE user_email_confirmations (
   user_id INT UNIQUE NOT NULL,
   token uuid UNIQUE NOT NULL,
+  email VARCHAR UNIQUE NOT NULL,
   created_at TIMESTAMPTZ NOT NULL,
-  updated_at TIMESTAMPTZ NOT NULL,
-  deleted_at TIMESTAMPTZ
+  updated_at TIMESTAMPTZ NOT NULL
 );
 COMMIT;
