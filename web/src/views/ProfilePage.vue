f95a23b9bae16237132083e564c1eb35f59f9ada
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-11-13 13:47:47 -0500

    webapp: test build netlify

--- web/src/views/ProfilePage.vue
+++ web/src/views/ProfilePage.vue
@@ -55,10 +55,12 @@
                     <span class="heading">{{ userModules.length }}</span>
                     <span class="description">Published Modules</span>
                   </div>
+                  <!--  -->
                   <!-- <div>
                     <span class="heading">0</span>
                     <span class="description">Followers</span>
                   </div> -->
+                  <!--  -->
                 </div>
               </div>
             </div>
