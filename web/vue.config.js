dcd1032af8de625d8481db61a9a61d6322f50af1
Marko Baricevic <marbar3778@yahoo.com>
2021-03-05 14:53:36 +0100

    set baseurl for router

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/REPO_NAME/'
+    : '/',
+    baseUrl:  process.env.NODE_ENV === 'production'
+    ? '/REPO_NAME/'
     : '/'
 }
